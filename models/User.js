7fa307e7b48ce306474858d4f4888b2deda9648a
Amar Enzy <enzyxmxr@gmail.com>
2025-03-31 08:41:54 +0200

    ajout USER dans la base
    modif du nom de la bdd
    ajout de la table user dans la bdd
    ajout de chose secondaire ( MLD)
    mise en place de l'API

---
+++ models/User.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+    idUser: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    },
+    email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    },
+    password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    nom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    prenom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    adresse: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    });
+    return User;
+};
